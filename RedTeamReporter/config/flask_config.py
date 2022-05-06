fb7f2c2296ffb3f55aa53c1c3a1d13edff3bc423
Wintermute <dai500k@hotmail.com>
2022-05-06 09:53:27 +0100

    Create flask_config.py

---
+++ RedTeamReporter/config/flask_config.py
@@ -0,0 +1,23 @@
+from datetime import timedelta
+
+class Config(object):
+	
+	DEBUG=True
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+	SECRET_KEY = "testsecretkey123123123"
+	JWT_SECRET_KEY='jwtsecretkey123'
+	JWT_ACCESS_TOKEN_EXPIRES=timedelta(hours=10)
+	JWT_REFRESH_TOKEN_EXPIRES=timedelta(days=30)
+	
+class ProdConfig(Config):
+	pass
+
+class DevConfig(Config):
+	
+	DEBUG=False
+	SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+	SECRET_KEY = "testsecretkey123123123"
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	JWT_SECRET_KEY='jwtsecretkey123'
+	JWT_ACCESS_TOKEN_EXPIRES=timedelta(hours=10)
