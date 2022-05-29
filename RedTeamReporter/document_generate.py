7b5a392a9acfc43da6c8977085ea1999deabb739
W1ntrMut3 <dai500k@hotmail.com>
2022-05-29 18:25:35 +0100

    comment

--- RedTeamReporter/document_generate.py
+++ RedTeamReporter/document_generate.py
@@ -2,6 +2,16 @@ from flask import Blueprint
 
 generate = Blueprint("generate", __name__, url_prefix="/api/v1/generate")
 
+
+# Here I need to get all of the data that I need to generate a report
+# order it in a way that each issue cascades from high (first) to low (last) in priority of severity
+# I then need to output it to the document using a template
+# I need the template to be able to output entire "issue" chunks mutliple times
+#
+#
+#
+#
+
 @generate.post('/<int:engagement_id>/generate')
 @jwt_required()
 def register():
